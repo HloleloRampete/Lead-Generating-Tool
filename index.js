2f2d9ec0775fac8a03949f75cdd87941e44b3de6
Hlolelo Rampete <156667505+HloleloRampete@users.noreply.github.com>
2024-03-04 16:24:00 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
-})
